79f7b2718fc51bb52cd99cb97392ebb7a0027a65
Gopal <68118204+gopalsays@users.noreply.github.com>
2021-03-25 23:35:07 +0530

    Update Producer.java

--- LooseCouplingg/src/first/Producer.java
+++ LooseCouplingg/src/first/Producer.java
@@ -2,9 +2,9 @@ package first;
 
 public class Producer implements IWorker{
 	public void first() {
-		System.out.println("Pfirstsup");
+		System.out.println("this is first");
 	}
 	public  void second() {
-		System.out.println("Psecondsup");
+		System.out.println("this is second");
 	}
 }
